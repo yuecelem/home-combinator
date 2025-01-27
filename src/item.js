3e2d36e4e1e4d9580a6cf01f5ed4994188103d03
yuecelem <emre.ycl@hotmail.com>
2025-01-27 15:38:11 +0300

    Add edit methods to Item class

--- src/item.js
+++ src/item.js
@@ -1,13 +1,31 @@
 export default class Item{
-    constructor(id, brand, itemType, assignedSection, price, link) {
+    constructor(id, brand, itemType, assignedSectionName, price, link) {
         this.id = id;
         this.brand = brand;
         this.itemType = itemType;
-        this.assignedSection = assignedSection;
+        this.assignedSectionName = assignedSectionName;
         this.price = price;
         this.link = link;
     }
 
-    
+    changeItemBrand (newBrand) {
+        this.brand = newBrand;
+    }
+
+    changeItemType (newItemType) {
+        this.itemType = newItemType;
+    }
+
+    changeAssignedSectionName (newAssignedSectionName) {
+        this.assignedSectionName = newAssignedSectionName;
+    }
+
+    changePrice (newPrice) {
+        this.price = newPrice;
+    }
+
+    changeLink (newLink) {
+        this.link = newLink;
+    }
 };
 
